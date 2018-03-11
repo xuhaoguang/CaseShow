3b87ac2020ae114adec64a420ec8c406e146d4b5
xuhaoguang <hgxu1993@163.com>
2018-03-11 21:11:25 +0800

    add search.php

---
+++ search.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CaseShow 法务服务系统</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.html">
+                <img src="img/logo.png" >
+            </a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-collapse">
+                <span class="sr-only"> </span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="example-navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.html">首页</a></li>
+                <li class="dropdown active">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        文书检索 <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="search.php">语义检索</a></li>
+
+                        <!--<li class="divider"></li>-->
+                        <li><a href="#">案由检索</a></li>
+
+                        <!--<li class="divider"></li>-->
+                        <li><a href="#">法院检索</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">法律条文</a></li>
+                <li><a href="#">指导案例</a></li>
+                <li><a href="nlp-tool.php">NLP工具</a></li>
+                <li><a href="about.html">关于我们</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<main class="main">
+    <div class="row">
+        <div class="col-md-1"></div>
+        </div>
+
+        <div class="col-md-3">
+    </div>
+
+<footer class="panel-footer">
+    <br>
+    <a target="_blank" href="http://wenshu.court.gov.cn/">|中国裁判文书网|</a>
+    <a target="_blank" href="http://www.ajxxgk.jcy.cn/html/index.html">人民检察院案件信息公开网|</a><br>
+    <a target="_blank" href="http://www.court.gov.cn/zgsplcxxgkw/">|中国审判流程信息公开网|</a>
+    <a target="_blank" href="http://shixin.court.gov.cn/">中国执行信息公开网|</a>
+    <a target="_blank" href="http://ipr.court.gov.cn/">中国知识产权裁判文书网|</a>
+    <a target="_blank" href="http://www.ccmt.org.cn/">中国涉外商事海事审判网|</a>
+    <p>
+        地址：辽宁省沈阳市浑南区南屏东路16号 电话：024-24696180 <br>
+        中国科学院沈阳计算技术研究所 版权所有<br><br>
+        辽ICP备18002117号-1
+    </p>
+</footer>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+
